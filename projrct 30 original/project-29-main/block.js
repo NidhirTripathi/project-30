58fc67bcdefc9514303a3c46c7a3c6f564cb9edd
NidhirTripathi <71880428+NidhirTripathi@users.noreply.github.com>
2021-05-02 15:44:56 +0530

    Add files via upload

---
+++ projrct 30 original/project-29-main/block.js
@@ -0,0 +1,42 @@
+class Block {
+  constructor(x, y, width, height) {
+    var options = {
+      
+        'restitution':0.8,
+        'friction':0.3,
+        'density':0.5,
+        isStatic: false
+        
+    }
+    this.visiblity=255
+
+   this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    if(this.body.speed<8){
+      var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill("yellow");
+    rect(0, 0, this.width, this.height);
+    pop();
+
+    }
+    else {
+      World.remove(world,this.body)
+      push()
+this.visiblity=this.visiblity-5
+
+
+
+      pop()
+    }
+  }
+};
